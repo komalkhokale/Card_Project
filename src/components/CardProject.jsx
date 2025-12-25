d98be9a4e2b8f461adf2fcffc3848f5b851938e0
komalkhokale <kkhokale15@gmail.com>
2025-12-25 09:24:01 +0530

    Change Style

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-const CardProject = ({user}) => {
-
+const CardProject = ({ user }) => {
   return (
     <div className="flex items-center justify-baseline bg-[#111]">
       <div className="rounded shadow-lg w-40 px-2 py-2 bg-white h-auto m-3">
@@ -23,20 +22,32 @@ const CardProject = ({user}) => {
         </div>
 
         {/* User Info */}
-        <div className="mt-7 flex items-center justify-between">
+        <div className="mt-7 text-center">
           <h2 className="font-medium text-sm">{user.name}</h2>
-          <div className="flex items-center">
-            <i className="ri-heart-fill text-xs text-pink-600"></i>
-            <h2 className="text-[10px]">{user.likes}</h2>
-          </div>
         </div>
 
-        <h3 className="text-[7px] mb-1 mt-1 text-gray-700">{user.bio}</h3>
+        <h3 className="text-[7px] mb-1 mt-1 text-gray-700 text-center">{user.bio}</h3>
 
         {/* Followers and Following */}
-        <div>
-          <h2 className="text-[9px]">Followers: {user.followers}</h2>
-          <h2 className="text-[9px]">Following: {user.following}</h2>
+        <div className="flex justify-between text-center mt-3 mb-2 bg-gray-300 rounded px-1 py-1">
+          <h2 className="text-[7px]">
+            Likes <br />
+            {user.likes}
+          </h2>
+          <h2 className="text-[7px]">
+            Followers <br />
+            {user.followers}
+          </h2>
+          <h2 className="text-[7px]">
+            Following <br />
+            {user.following}
+          </h2>
+        </div>
+
+        <div className="flex justify-center text-gray-700 gap-2 mt-2">
+          <i className="text-[10px] ri-instagram-line"></i>
+          <i className="text-[10px] ri-telegram-2-line"></i>
+          <i className="text-[10px] ri-threads-line"></i>
         </div>
       </div>
     </div>
